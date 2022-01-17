d50b4ca403eb5b3c2deeb2bcd3b9d3157abcd6c9
benjamindobbs <54630929+benjamindobbs@users.noreply.github.com>
2022-01-16 20:16:23 -0500

    JS integration for repeated code
    Went buck, moved the nav bar, player, and fade to js files to neaten up the place

---
+++ js/fade.js
@@ -0,0 +1,6 @@
+document.body.classList.add('fade');
+        document.addEventListener("DOMContentLoaded", () => {
+            window.setTimeout(function () {
+                document.body.classList.remove('fade');
+            }, 230);
+        });
---
+++ js/navDrawer.js
@@ -0,0 +1,9 @@
+document.write( '<div id=\"mySidenav\" class=\"sidenav\">\n' );
+document.write( '        <a href=\"javascript:void(0)\" class=\"closebtn\" onclick=\"closeNav()\">x</a>\n' );
+document.write( '        <a href=\"index.html\">Home</a>\n' );
+document.write( '        <a href=\"track_placeholder.html\">Place Holder</a>\n' );
+document.write( '        <a href=\"page2.html\">Page 2</a>\n' );
+document.write( '        <a href=\"page3.html\">Page 3</a>\n' );
+document.write( '        <a href=\"page4.html\">Page 4</a>\n' );
+document.write( '    </div>\n' );
+document.write( '    <i class=\"material-icons\" onclick=\"openNav()\">menu</i>' );
