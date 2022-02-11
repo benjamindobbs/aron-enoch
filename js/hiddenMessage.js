1a96db67faf153e1091d334d5746c5c1b402bfa6
javel15 <javelst15@gmail.com>
2022-02-11 16:57:55 -0500

    finished references

--- js/hiddenMessage.js
+++ js/hiddenMessage.js
@@ -1,13 +1,13 @@
-let messages = ["/ghostMessages/message1.mp3","./assets/ghostMessages/message2.mp3","./assets/ghostMessages/message3.mp3","./assets/ghostMessages/message4.mp3","./assets/ghostMessages/message5.mp3","./assets/ghostMessages/message6.mp3","./assets/ghostMessages/message7.mp3"]
+let messages = ["../ghostMessages/message1.mp3","../assets/ghostMessages/message2.mp3","../assets/ghostMessages/message3.mp3","../assets/ghostMessages/message4.mp3","../assets/ghostMessages/message5.mp3","../assets/ghostMessages/message6.mp3","../assets/ghostMessages/message7.mp3"]
 let curr_track = document.createElement('audio');
-
+curr_track.src = messages[1];
 function loadTrack(trackNumber){
     curr_track.src = messages[trackNumber];
 }
 let isPlaying= false;
 curr_track.load();
 let playpause_btn = document.querySelector(".playpause-track");
-curr_track.addEventListener("ended", history.back());
+// curr_track.addEventListener("ended", history.back());
 
 function playpauseTrack() {
     // Switch between playing and pausing
